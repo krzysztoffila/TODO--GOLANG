d4fc7d5b87c9baf13ab4e4c7c6ba43345ff3f6c4
krzysztoffila <umowy.fila@gmail.com>
2024-07-04 21:48:14 +0200

    Testing log.Fatal

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	Id      int
+	Message string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	data := map[string][]Todo{
+		"Todos": {
+			Todo{
+				Id:      1,
+				Message: "Learn Go",
+			},
+		},
+	}
+
+	todosHandler := func(w http.ResponseWriter, r *http.Request) {
+		templ := template.Must(template.ParseFiles("index.html"))
+		templ.Execute(w, data)
+	}
+
+	http.HandleFunc("/", todosHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
