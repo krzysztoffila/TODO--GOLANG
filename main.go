26bf3e8007ca9dbe4e2b7121cf0a96e341083bf6
krzysztoffila <umowy.fila@gmail.com>
2024-07-05 11:29:33 +0200

    Finish todo

--- main.go
+++ main.go
@@ -15,25 +15,28 @@ func main() {
 
 	data := map[string][]Todo{
 		"Todos": {
-			Todo{
-				Id:      1,
-				Message: "Learn Go",
-			},
-			Todo{
-				Id:      2,
-				Message: "Learn HTMX",
-			},
+			{Id: 1, Message: "Learn Go"},
+			{Id: 2, Message: "Learn HTMX"},
 		},
 	}
 
 	todosHandler := func(w http.ResponseWriter, r *http.Request) {
-		templ := template.Must(template.ParseFiles("index.html"))
+		templ := template.Must(template.ParseFiles("index.html", "todo_list_element.html"))
 		templ.Execute(w, data)
+	}
+
+	addTodoHandler := func(w http.ResponseWriter, r *http.Request) {
+		message := r.PostFormValue("message")
+		templ := template.Must(template.ParseFiles("todo_list_element.html"))
+		todo := Todo{Id: len(data["Todos"]) + 1, Message: message}
+		data["Todos"] = append(data["Todos"], todo)
+		templ.ExecuteTemplate(w, "todo-list-element", todo)
 	}
 
 	http.HandleFunc("/", todosHandler)
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/add-todo", addTodoHandler)
 
 	log.Println("Starting server on :8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
